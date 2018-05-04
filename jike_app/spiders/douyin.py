9d774f0e200f31915d11292cc9af7d02627139ec
yangcongtougg <243835062@qq.com>
2018-05-04 18:48:42 +0800

    url

--- jike_app/spiders/douyin.py
+++ jike_app/spiders/douyin.py
@@ -24,13 +24,10 @@ class DouyinSpider(scrapy.Spider):
         'Connection': 'keep-alive',
         # 'Cookie': 'install_id=31717878106; odin_tt=61493451513970616d4d765441634375947e5e5648add0aa69c062e39cd72cd95aa376bdd5e8c1741579a6043d0086df; ttreq=1$5fe4fb1448254ccdfb47c989eced6965690bcf31'
     }
-
+    url = 'https://aweme.snssdk.com/aweme/v1/feed/?iid=31717878106&ac=WIFI&os_api=18&app_name=aweme&channel=App%20Store&idfa=80DEEA4B-2DC9-43FE-A097-4619DFBF8468&device_platform=iphone&build_number=17909&vid=787FF51A-8429-4C9D-93C1-27A6CD0AE7D8&openudid=93d55ca4a7fd3596e0ee7434610d3c1aac90c8f7&device_type=iPhone7,1&app_version=1.7.9&device_id=10258085598&version_code=1.7.9&os_version=9.3.2&screen_width=1125&aid=1128&count=6&feed_style=0&max_cursor=0&pull_type=2&type=0&volume=0.00&mas=0098aea8eacc0e2e63965ee05cf7e0b7dd9675ce7752c8782126a8&as=a145836e40163a89ec5217&ts=1525430624'
     def start_requests(self):
         for i in range(100000):
-            yield Request(url='https://aweme.snssdk.com/aweme/v1/feed/?iid=31717878106&ac=WIFI&os_api=18&app_name=aweme&channel=App%20Store&idfa=80DEEA4B-2DC9-43FE-A097-4619DFBF8468&device_platform=iphone&build_number=17909&vid=787FF51A-8429-4C9D-93C1-27A6CD0AE7D8&openudid=93d55ca4a7fd3596e0ee7434610d3c1aac90c8f7&device_type=iPhone7,1&app_version=1.7.9&device_id=10258085598&version_code=1.7.9&os_version=9.3.2&screen_width=1125&aid=1128&count=6&feed_style=0&max_cursor=0&pull_type=2&type=0&volume=0.00&mas=005b02de53fb5fdf37ee286c63517a73537c917776712366ee80c7&as=a175f25ee56fdab46c1872&ts=1525425397', method='GET',
-                          headers=self.default_headers,
-                          callback=self.parse,
-                          dont_filter=True)
+            yield Request(url=self.url, method='GET', headers=self.default_headers, callback=self.parse, dont_filter=True)
 
     def parse(self, response):
         meta_data = json.loads(response.body, encoding='utf-8')
