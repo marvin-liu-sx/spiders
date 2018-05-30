a6baa5de5f133bb4d7adf7a0dfbaa2f383b6e6b0
yangcongtougg <243835062@qq.com>
2018-05-30 19:11:55 +0800

    0530

--- jike_app/spiders/author_api_pear.py
+++ jike_app/spiders/author_api_pear.py
@@ -6,8 +6,8 @@ import uuid
 import random
 from ..items import MiaoPaiItem
 
-author_list = ['10117614', '10054243']
-
+author_list = ['11549086']
+TOPIC = '今日看历史'
 
 class RandomHeader(object):
     def __init__(self):
@@ -33,7 +33,6 @@ class RandomHeader(object):
         }
         return post_header
 
-
 class AuthorApiPearSpider(scrapy.Spider):
     name = 'author_api_pear'
     allowed_domains = ['www.pearvideo.com']
@@ -51,86 +50,90 @@ class AuthorApiPearSpider(scrapy.Spider):
             #print(json.dumps(json_data))
             if json_data['dataList']:
                 for i in json_data['dataList']:
-                    _s = random.sample([random.randint(1, 100000000000)], 1)
-                    _t = int(round(time.time() * 1000))
-                    i_id = _s[0] + _t
+                    if i['otype'] == '1':
+                        _s = random.sample([random.randint(1, 100000000000)], 1)
+                        _t = int(round(time.time() * 1000))
+                        i_id = _s[0] + _t
 
-                    channel_id = ''
-                    topic = '美食'
-                    question_type = ''
+                        channel_id = ''
+                        topic = TOPIC
+                        question_type = ''
 
-                    if i['contInfo']['userInfo']['nickname']:
-                        media_name = i['contInfo']['userInfo']['nickname']
-                    else:
-                        media_name = '暂无'
+                        if i['contInfo']['userInfo']['nickname']:
+                            media_name = i['contInfo']['userInfo']['nickname']
+                        else:
+                            media_name = '暂无'
 
-                    if i['contInfo']['userInfo']['userId']:
-                        media_id = i['contInfo']['userInfo']['userId']
-                    else:
-                        media_id = '暂无'
+                        if i['contInfo']['userInfo']['userId']:
+                            media_id = i['contInfo']['userInfo']['userId']
+                        else:
+                            media_id = '暂无'
 
-                    if i['contInfo']['name']:
-                        video_title = i['contInfo']['name']
-                    else:
-                        video_title = '暂无'
+                        if i['contInfo']['name']:
+                            video_title = i['contInfo']['name']
+                        else:
+                            video_title = '暂无'
 
-                    if i['contInfo']['contId']:
-                        video_id = i['contInfo']['contId']
-                    else:
-                        video_id = '暂无'
+                        if i['contInfo']['contId']:
+                            video_id = i['contInfo']['contId']
+                        else:
+                            video_id = '暂无'
 
-                    if i['contInfo']['praiseTimes']:
-                        play_count = i['contInfo']['praiseTimes']
-                    else:
-                        play_count = 0
+                        if i['contInfo']['praiseTimes']:
+                            play_count = i['contInfo']['praiseTimes']
+                        else:
+                            play_count = 0
 
-                    play_url = ''
-                    video_duration = 0
-                    if i['contInfo']['videos']:
-                        for v in i['contInfo']['videos']:
-                            if v['tag'] == 'ld':
-                                play_url = v['url']
-                                video_duration = v['duration']
-                            elif v['tag'] == 'sd':
-                                play_url = v['url']
-                                video_duration = v['duration']
-                            elif v['tag'] == 'hd':
-                                play_url = v['url']
-                                video_duration = v['duration']
-                            elif v['tag'] == 'fhd':
-                                play_url = v['url']
-                                video_duration = v['duration']
-                    else:
-                        play_url = '暂无'
+                        play_url = ''
                         video_duration = 0
+                        if i['contInfo']['videos']:
+                            for v in i['contInfo']['videos']:
+                                if v['tag'] == 'ld':
+                                    play_url = v['url']
+                                    video_duration = v['duration']
+                                elif v['tag'] == 'sd':
+                                    play_url = v['url']
+                                    video_duration = v['duration']
+                                elif v['tag'] == 'hd':
+                                    play_url = v['url']
+                                    video_duration = v['duration']
+                                elif v['tag'] == 'fhd':
+                                    play_url = v['url']
+                                    video_duration = v['duration']
+                        else:
+                            play_url = '暂无'
+                            video_duration = 0
 
-                    video_url = 'http://www.pearvideo.com/video_' + video_id
+                        video_url = 'http://www.pearvideo.com/video_' + video_id
 
-                    if i['contInfo']['pic']:
-                        video_cover = i['contInfo']['pic']
+                        if i['contInfo']['pic']:
+                            video_cover = i['contInfo']['pic']
+                        else:
+                            video_cover = '暂无'
+
+                        item = MiaoPaiItem()
+                        item['i_id'] = i_id
+                        item['channel_id'] = channel_id
+                        item['topic'] = topic
+                        item['question_type'] = question_type
+                        item['media_name'] = media_name
+                        item['media_id'] = media_id
+                        item['video_title'] = video_title
+                        item['video_id'] = video_id
+                        item['play_count'] = play_count
+                        item['play_url'] = play_url
+                        item['video_duration'] = video_duration
+                        item['video_url'] = video_url
+                        item['video_cover'] = video_cover
+                        item['source'] = 11
+                        item['status'] = 0
+                        item['meta_data'] = json.dumps(json_data, ensure_ascii=False).replace('\'', '').replace('\\"', '')
+                        item['video_width'] = 640
+                        item['video_height'] = 360
+                        yield item
+                        #print(item)
                     else:
-                        video_cover = '暂无'
-
-                    item = MiaoPaiItem()
-                    item['i_id'] = i_id
-                    item['channel_id'] = channel_id
-                    item['topic'] = topic
-                    item['question_type'] = question_type
-                    item['media_name'] = media_name
-                    item['media_id'] = media_id
-                    item['video_title'] = video_title
-                    item['video_id'] = video_id
-                    item['play_count'] = play_count
-                    item['play_url'] = play_url
-                    item['video_duration'] = video_duration
-                    item['video_url'] = video_url
-                    item['video_cover'] = video_cover
-                    item['source'] = 11
-                    item['status'] = 0
-                    item['meta_data'] = json.dumps(json_data, ensure_ascii=False).replace('\'', '').replace('\\"', '')
-                    item['video_width'] = 640
-                    item['video_height'] = 360
-                    yield item
+                        pass
 
             if json_data['nextUrl']:
                 print('Has More Data')
